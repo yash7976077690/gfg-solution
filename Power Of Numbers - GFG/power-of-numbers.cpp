5e0cbb69a4600ed862fd06338c9d61d914a9a8d1
yash7976077690 <126045388+yash7976077690@users.noreply.github.com>
2023-07-12 17:14:16 +0530

    Added solution - LeetHub

--- Power Of Numbers - GFG/power-of-numbers.cpp
+++ Power Of Numbers - GFG/power-of-numbers.cpp
@@ -6,13 +6,12 @@ using namespace std;
 
 class Solution{
     public:
-      int mod = 1000000007;
     //You need to complete this fucntion
-    
+      int mod = 1000000007;
     long long power(int N,int R)
     {
        //Your code here
-       if(R==1)return N%mod;
+         if(R==1)return N%mod;
        long long half = power(N,R/2);
        half = (half*half)%mod;
        if(R%2==1)half = (half*N)%mod;
